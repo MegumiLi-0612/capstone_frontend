ad6354f488a585e9e3c8dda4502b6140d7e7bec9
Megumi <huihaoli2025@u.northwestern.edu>
2025-08-28 11:52:59 +0800

    Update Header.js

--- jobmatch-react/src/components/Header.js
+++ jobmatch-react/src/components/Header.js
@@ -78,14 +78,7 @@ function Header() {
                   }}>
                     Dashboard
                   </Link>
-                  <Link to="/todo" style={{ 
-                    color: 'white', 
-                    textDecoration: 'none', 
-                    padding: '0.5rem 1rem', 
-                    borderRadius: '4px' 
-                  }}>
-                    My Tasks
-                  </Link>
+               
                 </>
               )}
               
@@ -132,3 +125,4 @@ function Header() {
 }
 
 export default Header;
+
